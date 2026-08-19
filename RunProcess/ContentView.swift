22a2828a5848c1f5271f988245469db3feefa3f7
CaoHaoran-Dev <caohaoranspare@icloud.com>
2026-08-19 19:35:54 +0800

    Initial Commit

---
+++ RunProcess/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RunProcess
+//
+//  Created by Haoran on 2026/8/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RunProcess/RunProcessApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunProcessApp.swift
+//  RunProcess
+//
+//  Created by Haoran on 2026/8/19.
+//
+
+import SwiftUI
+
+@main
+struct RunProcessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
